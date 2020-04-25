b38cc9da2c2aa56ad5ecda329e763c1e2d8d0885
Igor <igormcsouza@gmail.com>
2020-04-25 10:33:54 -0300

    updated

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -1,5 +1,5 @@
 docker run --rm -it \
 --network='host' \
--v `pwd`/notebook:/workdir/notebook \
+-v `pwd`/notebooks:/workdir/notebooks \
 igormcsouza/python-opt-tools:pyomo-datapeste \
 jupyter notebook --allow-root
